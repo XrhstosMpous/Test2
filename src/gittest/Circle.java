5c4648d922c4f51cac23de821e477bc8363fb165
mpousxristos <xristosmpousmpouras@gmail.com>
2022-05-04 20:34:14 +0300

    adding a circle class

---
+++ src/gittest/Circle.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author PSO
+ */
+public class Circle {
+    
+    private int x;
+    private int y;
+    private int z;
+    
+}
